1ac182ec2d3bcc2bd6f2c6ada9842ea0057c01c4
gabriela <isabel.cmsp@gmail.com>
2025-04-22 19:05:14 -0300

    a data de nascimento era limitada ate 2015

--- login.py
+++ login.py
@@ -3,11 +3,11 @@ st.title("Cadastro de clientes")
 
 nome = st.text_input("digite o nome do cliente:")
 endereco = st.text_input("digite o endereço do cliente:")
-data_de_nascimento = st.date_input("Escolha a data de nascimento:")
+data_de_cadastro = st.date_input("Escolha a data de cadastro:")
 tipo_cliente = st.selectbox("tipo do cliete",["Pessoa física","Pessoa juridica"])
 botao_cadastro = st.button("cadastrar cliente")
 
 if botao_cadastro:
     with open("clientes.csv","a") as arquivo:
-        arquivo.write(f"{nome},{endereco},{data_de_nascimento},{tipo_cliente}")
+        arquivo.write(f"{nome},{endereco},{data_de_cadastro},{tipo_cliente}")
         st.success("cliente cadastrado com sucesso ")
